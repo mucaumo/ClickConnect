5e6ec7e4f8fcb5105b1c6948414a36722db09e2f
lucasrychlicki <lucasrychlicki@hotmail.com>
2024-03-06 22:05:45 -0300

    Remoção do método update

--- src/main/java/br/com/fiap/clickconnect/controller/MouseController.java
+++ src/main/java/br/com/fiap/clickconnect/controller/MouseController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,17 +66,6 @@ public class MouseController {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
-    @PutMapping("/{id}")
-    public ResponseEntity<Mouse> update(@PathVariable Long id){
-        log.info("Atualizando mouse com id{}");
-
-        for(Mouse mouse:mouseRepository){
-            if (mouse.id().equals(id))
-                return ResponseEntity.status(HttpStatus.OK).body(mouse);
-        }
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-    }
 
 }
 
